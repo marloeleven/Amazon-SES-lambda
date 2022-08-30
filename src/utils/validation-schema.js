f0338ab5249753e375c510d955597a1b0bd3f0b0
Marlo Dela Torre <mato@splitmedialabs.com>
2022-08-30 15:57:04 +0800

    validation schema adjustment

--- src/utils/validation-schema.js
+++ src/utils/validation-schema.js
@@ -3,12 +3,12 @@ import Joi from 'joi';
 export const emailSchema = Joi.object({
   fromName: Joi.string().trim().min(3).max(30).required(),
   recipients: Joi.array().items(Joi.string().trim().email()),
-  subject: Joi.string().trim().min(3).max(30).required(),
-  html: Joi.string().trim().min(1).max(30).required(),
+  subject: Joi.string().trim().min(3).max(50).required(),
+  html: Joi.string().trim().min(1).required(),
 });
 
 export const templateSchema = Joi.object({
   name: Joi.string().trim().min(3).max(30).required(),
-  subject: Joi.string().trim().min(3).max(30).required(),
-  html: Joi.string().trim().min(3).max(30).required(),
+  subject: Joi.string().trim().min(3).max(50).required(),
+  html: Joi.string().trim().min(3).required(),
 });
